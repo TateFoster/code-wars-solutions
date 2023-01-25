c24677d7b7c322ca63d643ccb6dde794dbf26e15
Dragon Lore <tate.j.foster@gmail.com>
2023-01-25 16:28:16 -0700

    running into problem with defining punctuation

--- simple-pig-latin.js
+++ simple-pig-latin.js
@@ -3,16 +3,20 @@ function pigIt(str) {
 	let words = str.split(" ");
 	const newWords = [];
 	for (let word of words) {
-		let splitWord = word.split("");
-		let moveLetter = splitWord.shift();
-		splitWord.push(moveLetter);
-		splitWord.push("ay");
-		let newWord = splitWord.join("");
-		newWords.push(newWord);
+		if (word == /[.,\/#!$%&\^\*;:{}=\-_`~()]/) {
+			newWords.push(word);
+		} else {
+			let splitWord = word.split("");
+			let moveLetter = splitWord.shift();
+			splitWord.push(moveLetter);
+			splitWord.push("ay");
+			let newWord = splitWord.join("");
+			newWords.push(newWord);
+		}
 	}
 	return newWords.join(" ");
 }
 
-console.log(pigIt("Test the pig latin"));
+console.log(pigIt("Test the pig latin !"));
 
 // To move the first letter of each word to the end and add "ay" to the end of the word.
